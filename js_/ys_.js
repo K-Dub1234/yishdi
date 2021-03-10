c39d63821817b35e39a9e124d751d861aff4cb83
K-Dub1234 <75134550+K-Dub1234@users.noreply.github.com>
2021-03-10 11:44:07 -0700

    1.3.0
    ...Finally

---
+++ js_/ys_.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+function openWin() {
+  window.open("https://github.com/K-Dub1234/yishdi");
+}
+</script>
+</head>
+<body>
+
+<form>
+  <input type="button" value="See it on GitHub" onclick="openWin()">
+</form>
+
+</body>
+</html>
